bae54ef33d1a935a18499718f77a49e4ca624411
Justin Barrick <justin.m.barrick@gmail.com>
2018-06-17 03:24:47 +0000

    Fix SSH key generation

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -25,6 +25,11 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 		objects := flux.FluxRoles(o)
 		objects = append(objects, flux.NewFluxPod(o))
 
+		err := sdk.Get(flux.NewFluxSSHKey(o))
+		if err != nil {
+			objects = append(objects, flux.NewFluxSSHKey(o))
+		}
+
 		for _, object := range objects {
 			err := sdk.Create(object)
 			if err != nil && !errors.IsAlreadyExists(err) {
