a7742f258ac26462093ae64710e174f643528fb0
Your Name <you@example.com>
2018-06-18 00:36:29 +0000

    #7 Add validation for Flux CRD.

---
+++ cmd/flux-operator-crd-gen/main.go
@@ -0,0 +1,27 @@
+// https://github.com/ant31/crd-validation
+
+package main
+
+import (
+	"flag"
+	v1alpha1 "github.com/justinbarrick/flux-operator/pkg/apis/flux/v1alpha1"
+	crdutils "github.com/ant31/crd-validation/pkg"
+	"os"
+)
+
+var (
+	cfg crdutils.Config
+)
+
+func init() {
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagset = crdutils.InitFlags(&cfg, flagset)
+	flagset.Parse(os.Args[1:])
+}
+
+func main() {
+	cfg.GetOpenAPIDefinitions = v1alpha1.GetOpenAPIDefinitions
+	crd := crdutils.NewCustomResourceDefinition(cfg)
+	crdutils.MarshallCrd(crd, cfg.OutputFormat)
+	os.Exit(0)
+}
