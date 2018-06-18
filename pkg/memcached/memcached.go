ddb0ee6e62691559b55a61e8578157b37bc4065a
Justin Barrick <justin.m.barrick@gmail.com>
2018-06-18 07:18:38 +0000

    Properly handle updates #5

--- pkg/memcached/memcached.go
+++ pkg/memcached/memcached.go
@@ -5,6 +5,7 @@ import (
 	"github.com/justinbarrick/flux-operator/pkg/apis/flux/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"github.com/justinbarrick/flux-operator/pkg/utils"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -37,42 +38,56 @@ func NewMemcachedService(cr *v1alpha1.Flux) *corev1.Service {
 	}
 }
 
-// NewMemcachedPod creates a new memcached pod
-func NewMemcachedPod(cr *v1alpha1.Flux) *corev1.Pod {
+// NewMemcachedDeployment creates a new memcached deployment
+func NewMemcachedDeployment(cr *v1alpha1.Flux) *extensions.Deployment {
 	memcachedImage := utils.Getenv("MEMCACHED_IMAGE", "memcached")
 	memcachedVersion := utils.Getenv("MEMCACHED_VERSION", "1.4.25")
 
-	meta := utils.NewObjectMeta(cr, MemcachedName(cr))
-	meta.Labels = map[string]string{
+	labels := map[string]string{
 		"name": MemcachedName(cr),
 	}
+	meta := utils.NewObjectMeta(cr, MemcachedName(cr))
+	meta.Labels = labels
 
-	return &corev1.Pod{
+	replicas := int32(1)
+
+	return &extensions.Deployment{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pod",
-			APIVersion: "v1",
+			Kind:       "Deployment",
+			APIVersion: "extensions/v1beta1",
 		},
 		ObjectMeta: meta,
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "memcached",
-					Image:   fmt.Sprintf("%s:%s", memcachedImage, memcachedVersion),
-					ImagePullPolicy: "IfNotPresent",
-					Args: []string{"-m 64", "-p 11211", "-vv"},
-					Ports: []corev1.ContainerPort{
-						corev1.ContainerPort{
-							ContainerPort: 11211,
-						},
-					},
-					Resources: corev1.ResourceRequirements{
-						Limits: corev1.ResourceList{
-							corev1.ResourceMemory: resource.MustParse("512Mi"),
-							corev1.ResourceCPU: resource.MustParse("500m"),
-						},
-						Requests: corev1.ResourceList{
-							corev1.ResourceMemory: resource.MustParse("256Mi"),
-							corev1.ResourceCPU: resource.MustParse("500m"),
+		Spec: extensions.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "memcached",
+							Image:   fmt.Sprintf("%s:%s", memcachedImage, memcachedVersion),
+							ImagePullPolicy: "IfNotPresent",
+							Args: []string{"-m 64", "-p 11211", "-vv"},
+							Ports: []corev1.ContainerPort{
+								corev1.ContainerPort{
+									ContainerPort: 11211,
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("512Mi"),
+									corev1.ResourceCPU: resource.MustParse("500m"),
+								},
+								Requests: corev1.ResourceList{
+									corev1.ResourceMemory: resource.MustParse("256Mi"),
+									corev1.ResourceCPU: resource.MustParse("500m"),
+								},
+							},
 						},
 					},
 				},
@@ -83,5 +98,5 @@ func NewMemcachedPod(cr *v1alpha1.Flux) *corev1.Pod {
 
 // Create all of the resources necessary to create a memcached instance.
 func NewMemcached(cr *v1alpha1.Flux) []runtime.Object {
-	return []runtime.Object{NewMemcachedPod(cr), NewMemcachedService(cr)}
+	return []runtime.Object{NewMemcachedDeployment(cr), NewMemcachedService(cr)}
 }
